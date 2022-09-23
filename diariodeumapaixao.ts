e452d5184c3ea8e972346465561425fdb7e244ae
Marianasantos142 <111981338+Marianasantos142@users.noreply.github.com>
2022-09-23 12:07:36 -0300

    diariodeumapaixao

---
+++ diariodeumapaixao.ts
@@ -0,0 +1,10 @@
+import { Netflix } from "./netflix";
+
+export class DiarioDeUmaPaixao extends Netflix {
+
+}
+ 
+var diarioDeUmaPaixao = new DiarioDeUmaPaixao("DiarioDeUmaPaixao",2004,"Na década de 40, na Carolina do Sul, o operário Noah Calhoun e a rica Allie se apaixonam desesperadamente, mas os pais da jovem não aprovam o namoro. Noah então é enviado para combater na Segunda Guerra Mundial, e parece ser o fim do romance. Enquanto isso, Allie se envolve com outro homem. No entanto, se torna claro que a paixão deles ainda não acabou quando Noah retorna para a pequena cidade anos mais tarde, próximo ao casamento de Allie.",
+"romance",14,123);
+diarioDeUmaPaixao.mostrar();
+
