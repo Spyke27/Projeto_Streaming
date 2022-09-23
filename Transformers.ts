77ac96f0e50b37ee88a25d2661ebb940275d502b
Raphaelsants <111981321+Raphaelsants@users.noreply.github.com>
2022-09-23 12:05:33 -0300

    Transformers

---
+++ Transformers.ts
@@ -0,0 +1,10 @@
+import { Netflix } from "./netflix";
+
+export class Transformers extends Netflix{
+}
+export var robots = new Transformers("Transformers", 2007, "O destino da humanidade está em jogo quando duas raças de robôs, os Autobots e os vilões Decepticons, chegam à Terra. Os robôs possuem a capacidade de se transformarem em diferentes objetos mecânicos enquanto buscam a chave do poder supremo com a ajuda do jovem Sam.", "Ação, Ficcção Científica, Aventura, Suspense", 10, 2.24 );
+robots.mostrar();
+
+
+
+                                                                                            //Raphael Santos.
