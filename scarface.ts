68eac1cc3f57d72ea5aa90945811fad42d3f3637
Erickbellot12 <111981561+Erickbellot12@users.noreply.github.com>
2022-09-23 12:06:10 -0300

    scarface

---
+++ scarface.ts
@@ -0,0 +1,9 @@
+import { Netflix } from "./netflix";
+
+export class scarface extends Netflix {
+}
+ var tony = new Netflix("SCARFACE",1983,"Após receber residência permanente nos Estados Unidos em troca do assassinato de um oficial do governo cubano, Tony Montana se torna o chefe do tráfico de drogas em Miami. Matando qualquer um que entre em seu caminho, Tony eventualmente se torna o maior traficante da Flórida, controlando quase toda a cocaína que entra em Miami. Porém, a pressão da polícia, as guerras com cartéis colombianos e sua própria paranoia servem para alimentar as chamas de sua eventual queda.","crime/drama",18,2.50)
+
+ tony.mostrar();
+
+ //bellot
